98ec3f717e85d44c336b45907d032b82adf77c0f
Asaf Shmaya <sh_asaf@hotmail.com>
2022-04-29 13:53:38 +0300

    Added sorting OrderBy

---
+++ TT.Models/InvoiceParameters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TT.Models
+{
+    public class InvoiceParameters : QueryStringParameters
+    {
+        public InvoiceParameters()
+        {
+            OrderBy = "Number";
+        }
+        public DateTime MinCreated { get; set; }
+        public DateTime MaxCreated { get; set; } = DateTime.Now;
+        public DateTime MinChanged { get; set; }
+        public DateTime MaxChanged { get; set; } = DateTime.Now;
+        public Enums.Processing.Statuses ProcessingStatus { get; set; }
+        public double MinAmount { get; set; }
+        public double MaxAmount { get; set; }
+        public Enums.Payment.Methods PaymentMethod { get; set; }
+    }
+}
--- TT.WebAPI/Controllers/InvoiceController.cs
+++ TT.WebAPI/Controllers/InvoiceController.cs
@@ -2,9 +2,11 @@
 using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
+using System.Linq.Dynamic.Core;
 using System.Net;
+using System.Reflection;
+using System.Text;
 using TT.Models;
-using TT.WebAPI.App_Code;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,7 +17,7 @@ namespace TT.WebAPI.Controllers
     public class InvoiceController : ControllerBase
     {
         [HttpGet]
-        [Route("AllPaged")]
+        [Route("allPaged")]
         [ProducesResponseType(typeof(IEnumerable<Invoice>), (int)HttpStatusCode.OK)]
         public IActionResult GetAll(int page = 1, int pageSize = 50)
         {
@@ -47,28 +49,60 @@ namespace TT.WebAPI.Controllers
         [ProducesResponseType(typeof(Invoice), (int)HttpStatusCode.OK)]
         public IActionResult Get(int id)
         {
-            IEnumerable<Invoice>? invoice = GetPagedInvoices(1,int.MaxValue).Where(x => x.Number == id);
+            IEnumerable<Invoice>? invoice = GetPagedInvoices(1, int.MaxValue).Where(x => x.Number == id);
 
             return Ok(invoice);
         }
 
         [HttpGet]
-        [Route("filter")]
-        public ActionResult<Invoice[]> GetFiltered([FromQuery] Filter filter, int page, int pageSize)
+        [Route("getByParameters")]
+        public ActionResult<Invoice[]> GetByParameters([FromQuery] InvoiceParameters invoiceParameters)
         {
-            IEnumerable<Invoice>? invoice = GetPagedInvoices(page, pageSize);
+            var invoice = GetPagedInvoices(invoiceParameters.PageNumber, invoiceParameters.PageSize);
 
-            var filtered = invoice.Where(x => x.Created >= filter.MinCreated &&
-                                              x.Created <= filter.MaxCreated &&
-                                              x.Changed >= filter.MinChanged &&
-                                              x.Changed <= filter.MaxChanged &&
-                                              x.ProcessingStatus == filter.ProcessingStatus &&
-                                              x.Amount >= filter.MinAmount &&
-                                              x.Amount <= filter.MaxAmount &&
-                                              x.PaymentMethod == filter.PaymentMethod);
+            IEnumerable<Invoice> invoicesByParameters = Filter(invoiceParameters, invoice);
 
-            return Ok(filtered);
-    }
+            if (!string.IsNullOrWhiteSpace(invoiceParameters.OrderBy))
+                invoicesByParameters = OrderBy(invoiceParameters, invoicesByParameters);
+
+            return Ok(invoicesByParameters);
+        }
+
+        private static IEnumerable<Invoice> OrderBy(InvoiceParameters invoiceParameters, IEnumerable<Invoice> invoicesByParameters)
+        {
+            var orderParams = invoiceParameters.OrderBy.Trim().Split(',');
+            var propertyInfos = typeof(Invoice).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+                if (objectProperty == null)
+                    continue;
+                var sortingOrder = param.EndsWith(" desc") ? "descending" : "ascending";
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {sortingOrder}, ");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            invoicesByParameters = invoicesByParameters.AsQueryable().OrderBy(orderQuery);
+            return invoicesByParameters;
+        }
+
+        private static IEnumerable<Invoice> Filter(InvoiceParameters invoiceParameters, IEnumerable<Invoice> invoice)
+        {
+            return invoice.Where(x => x.Created >= invoiceParameters.MinCreated &&
+                                              x.Created <= invoiceParameters.MaxCreated &&
+                                              x.Changed >= invoiceParameters.MinChanged &&
+                                              x.Changed <= invoiceParameters.MaxChanged &&
+                                              x.ProcessingStatus == invoiceParameters.ProcessingStatus &&
+                                              x.Amount >= invoiceParameters.MinAmount &&
+                                              x.Amount <= invoiceParameters.MaxAmount &&
+                                              x.PaymentMethod == invoiceParameters.PaymentMethod);
+        }
 
         /* CANNED VISUAL STUDIO EXAMPLES */
 
